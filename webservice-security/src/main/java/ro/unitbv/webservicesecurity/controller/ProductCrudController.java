fd999c11a4f2af22d5613feed2540d79d3140f07
vlad fernoaga <contact@fernoaga.com>
2024-03-01 12:48:17 +0200

    add get product operation

--- webservice-security/src/main/java/ro/unitbv/webservicesecurity/controller/ProductCrudController.java
+++ webservice-security/src/main/java/ro/unitbv/webservicesecurity/controller/ProductCrudController.java
@@ -6,8 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,14 +27,24 @@ public class ProductCrudController {
     private ProductCrudService productCrudService;
 
     @PostMapping("/product")
-    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequestDto productRequestDto){
-        var productResponse = productCrudService.createProduct(productRequestDto.name(),productRequestDto.description());
-        return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
+    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequestDto productRequestDto) {
+        var productResponse = productCrudService.createProduct(productRequestDto.name(), productRequestDto.description());
+        return ResponseEntity.status(HttpStatus.CREATED)
+          .body(productResponse);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable("id") String id) {
+        return productCrudService.getProduct(id)
+          .map(productResponse -> ResponseEntity.ok(productResponse))
+          .orElse(ResponseEntity.notFound()
+            .build());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> validationExceptionHandler(MethodArgumentNotValidException ex){
-        return ResponseEntity.badRequest().body("Requested body is not valid");
+    public ResponseEntity<String> validationExceptionHandler(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest()
+          .body("Requested body is not valid");
     }
 }
--- webservice-security/src/main/java/ro/unitbv/webservicesecurity/service/ProductCrudService.java
+++ webservice-security/src/main/java/ro/unitbv/webservicesecurity/service/ProductCrudService.java
@@ -2,6 +2,7 @@ package ro.unitbv.webservicesecurity.service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -12,11 +13,19 @@ import ro.unitbv.webservicesecurity.dto.ProductResponse;
 @Service
 public class ProductCrudService {
 
-    private static final Map<UUID, Product> IN_MEMORY_PRODUCT_STORE = new HashMap<>();
+    private static final Map<String, Product> IN_MEMORY_PRODUCT_STORE = new HashMap<>();
 
     public ProductResponse createProduct(String name, String description) {
         var product = new Product(UUID.randomUUID(),name,description);
-        IN_MEMORY_PRODUCT_STORE.put(product.id(), product);
+        IN_MEMORY_PRODUCT_STORE.put(product.id().toString(), product);
         return new ProductResponse(product.id().toString(), product.name(),product.description());
+    }
+
+    public Optional<ProductResponse> getProduct(String id){
+        var product = IN_MEMORY_PRODUCT_STORE.get(id);
+        if(product != null){
+            return Optional.of(new ProductResponse(product.id().toString(),product.name(), product.description()));
+        }
+        return Optional.empty();
     }
 }
