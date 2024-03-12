d11a5e6bc2e37354c0e54d48afed5e16209dbc02
vlad fernoaga <contact@fernoaga.com>
2024-03-12 12:22:59 +0200

    configure in-memory users and configure the authentication manager

--- webservice-security/src/main/java/ro/unitbv/webservicesecurity/config/WebserviceConfig.java
+++ webservice-security/src/main/java/ro/unitbv/webservicesecurity/config/WebserviceConfig.java
@@ -2,19 +2,59 @@ package ro.unitbv.webservicesecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebserviceConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auth) -> auth.anyRequest()
-            .permitAll())
-          .csrf((csrf) -> csrf.disable());
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+          .authorizeHttpRequests(auth -> auth.anyRequest()
+            .authenticated())
+          .authenticationManager(authenticationManager)
+          .httpBasic((c) -> c.init(http));
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) throws Exception {
+        var authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+          .passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("admin")
+          .password(passwordEncoder.encode("admin"))
+          .roles("READ", "EDIT", "DELETE")
+          .build());
+        manager.createUser(User.withUsername("user")
+          .password(passwordEncoder.encode("user"))
+          .roles("READ")
+          .build());
+        manager.createUser(User.withUsername("editor")
+          .password(passwordEncoder.encode("editor"))
+          .roles("READ", "EDIT")
+          .build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
