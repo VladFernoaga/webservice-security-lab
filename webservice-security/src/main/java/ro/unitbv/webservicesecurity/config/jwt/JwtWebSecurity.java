a73ab67c6177437fb0957e23a184a1a0d8aab1af
vlad fernoaga <contact@fernoaga.com>
2024-04-09 12:05:38 +0300

    prepare to authenticate each endpoint with JWT
    implement JwtRequestFilter and config it as to be called before the UserNameAndPassword Authentication

--- webservice-security/src/main/java/ro/unitbv/webservicesecurity/config/jwt/JwtWebSecurity.java
+++ webservice-security/src/main/java/ro/unitbv/webservicesecurity/config/jwt/JwtWebSecurity.java
@@ -1,5 +1,6 @@
 package ro.unitbv.webservicesecurity.config.jwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,19 +16,23 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class JwtWebSecurity {
 
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-          .authorizeHttpRequests(auth -> auth
+          .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST,"/auth/login/**").permitAll()
             .anyRequest()
-            .permitAll())
+            .authenticated())
           .authenticationManager(authenticationManager)
-          .sessionManagement( sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+          .sessionManagement( sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+          .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class); // set JwtRequestFilter before Spring calls the usernamePasswordAuthFilter
         return http.build();
     }
 
